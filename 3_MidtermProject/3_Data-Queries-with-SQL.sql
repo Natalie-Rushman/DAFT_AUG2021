df2b32b6c7f3cd621af5930e039798ab424d293a
Natalie-Rushman <ana-di.123@hotmail.com>
2021-09-09 07:12:30 +0200

    Update Week 5, Day 4

--- 3_MidtermProject/3_Data-Queries-with-SQL.sql
+++ 3_MidtermProject/3_Data-Queries-with-SQL.sql
@@ -4,15 +4,15 @@ SELECT COUNT(*) FROM house_price_data; /* 21.597 observations */
 
 SELECT DISTINCT bathrooms, COUNT(*) AS nbr_bathrooms FROM house_price_data
 GROUP BY bathrooms
-ORDER BY bathrooms ASC;
+ORDER BY bathrooms ASC; /* Most houses (9362) are provided with 3 bathrooms. */
 
 SELECT DISTINCT bedrooms, COUNT(*) AS nbr_bedrooms FROM house_price_data
 GROUP BY bedrooms
-ORDER BY nbr_bedrooms DESC;
+ORDER BY nbr_bedrooms DESC; /* Most houses (824) are provided with 3 bedrooms. */
 
 SELECT DISTINCT house_floors, COUNT(*) AS nbr_floors FROM house_price_data
 GROUP BY house_floors
-ORDER BY nbr_floors ASC;
+ORDER BY nbr_floors ASC; /* Most houses (10673) consist of one floor. */
 
 SELECT DISTINCT kcgs_condition, COUNT(*) AS assign_condition FROM house_price_data
 GROUP BY kcgs_condition
@@ -23,9 +23,9 @@ GROUP BY kcgs_grade
 ORDER BY kcgs_grade ASC; /* Most houses are to be found in grade 6 through 10, the other grade are neglectable */
 
 SELECT house_id, price FROM house_price_data
-ORDER BY price ASC LIMIT 10;
+ORDER BY price ASC LIMIT 10; /* The cheapest house: 78.000 USD */
 
-SELECT AVG(price) FROM house_price_data; /* 540.296 USD */
+SELECT AVG(price) FROM house_price_data; /* The average cost: 540.296 USD */
 
 SELECT DISTINCT bedrooms, AVG(price) AS middle_price FROM house_price_data
 GROUP BY bedrooms
@@ -36,7 +36,7 @@ GROUP BY bedrooms
 ORDER BY bedrooms DESC; /* largest living area of 853 m2 with 8 bedrooms */
 
 SELECT DISTINCT kcgs_waterfront, AVG(price) AS middle_price_with_waterfront FROM house_price_data
-GROUP BY kcgs_waterfront; /* 1662525/531762 >>> houses with waterfront 3 times more expansive than without waterfront */
+GROUP BY kcgs_waterfront; /* 1662525/531762 >>> houses with waterfront 3 times more expensive than without waterfront */
 
 /* 
     Number of bedrooms either 3 or 4
@@ -49,13 +49,13 @@ GROUP BY kcgs_waterfront; /* 1662525/531762 >>> houses with waterfront 3 times m
 */
 
 SELECT house_id, bathrooms, bedrooms, kcgs_waterfront, kcgs_grade, kcgs_condition, house_floors, price FROM house_price_data
-WHERE (KCGS_waterfront = 0) AND (bedrooms BETWEEN 3 AND 4) AND (bathrooms > 3) AND (house_floors = 1) 
-	AND (kcgs_condition >= 3) AND (kcgs_grade >=5) AND price < 300000;
+WHERE (KCGS_waterfront = 0) AND (bedrooms BETWEEN 3 AND 4) AND (bathrooms > 2) AND (house_floors = 1) 
+	AND (kcgs_condition >= 3) AND (kcgs_grade >=5) AND price < 300000; /* No house available, change parameters: Plenty of affordable hauses with e.g. 3 bathrooms */
 
 /* list of properties whose prices are twice more than the average */
 /* Create a view Houses_with_higher_than_double_average_price */
 
-SELECT price FROM (SELECT price FROM house_price_data
+SELECT price FROM house_price_data
 WHERE price > (SELECT price FROM house_price_data WHERE price = 2*AVG(price));
 
 SELECT bedrooms, AVG(price) AS middle_price FROM house_price_data
@@ -66,10 +66,7 @@ SELECT COUNT(DISTINCT(zip_code)) FROM house_price_data; /* 70 zipcodes */
 
 SELECT DISTINCT zip_code, COUNT(*) AS houses_per_area FROM house_price_data
 GROUP BY zip_code
-ORDER BY houses_per_area DESC;
-
-SELECT zip_code FROM house_price_data
-WHERE zip_code = 98001;
+ORDER BY houses_per_area ASC; /* Most houses in 98103 (602), least in 98039 (50) */
 
 SELECT DISTINCT year_construction, AVG(price) AS middle_price FROM house_price_data
 GROUP BY year_construction
@@ -94,3 +91,19 @@ GROUP BY zip_code, counties.county_name
 ORDER BY middle_price DESC;
 
 SELECT * FROM counties;
+
+SELECT zip_code, AVG(price) AS middle_priced_area FROM house_price_data
+GROUP BY zip_code
+HAVING expensive_area > 900000
+ORDER BY expensive_area DESC;
+
+SELECT zip_code, AVG(price) AS middle_priced_area FROM house_price_data
+GROUP BY zip_code
+HAVING middle_priced_area <= 900000 AND middle_priced_area > 400000
+ORDER BY middle_priced_area DESC;
+
+SELECT zip_code, AVG(price) AS low_priced_area FROM house_price_data
+GROUP BY zip_code
+HAVING low_priced_area <= 400000
+ORDER BY low_priced_area DESC;
+
